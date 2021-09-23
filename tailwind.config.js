03bf83bc29522225fe8cf19a1ddb92f822c9d6a9
dacehell <caldera.david78@gmail.com>
2021-09-23 19:44:12 -0300

    navbar and header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      "gray-imdb": "#1F1F1F",
+      "blue-imdb": "#388CEF",
+      "red-imdb": "#FB4B4B",
+      "gray-light-imdb": "#313131",
+    }),
+    fontFamily: {
+      search: ['"Font Awesome 5 Free"'],
+      roboto: ["Roboto"],
+    },
+    extend: {
+      backgroundImage: {
+        "icon-search": "url('/public/assets/search.png')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
